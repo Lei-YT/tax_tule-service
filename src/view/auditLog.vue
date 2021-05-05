a266010e857ca9ae174ffa5ede10ae6fc62290c6
lyt <ldj_yt@outlook.com>
2021-05-06 01:05:35 +0800

    table footer

--- src/view/auditLog.vue
+++ src/view/auditLog.vue
@@ -9,86 +9,96 @@
           class="demo-form-inline"
           ref="formInline"
         >
-          <FormItem label="单据编号:" prop="code">
-            <Input v-model="formInline.code" placeholder="请输入单据编号" />
-          </FormItem>
-          <FormItem label="业务名称:" prop="type">
-            <Input v-model="formInline.type" placeholder="请输入业务名称" />
-          </FormItem>
-          <FormItem label="审核结果:" prop="checkResult">
-            <Select v-model="model1" style="width: 200px">
-              <Option
-                v-for="item in cityList"
-                :value="item.value"
-                :key="item.value"
-                >{{ item.label }}</Option
-              >
-            </Select>
-          </FormItem>
-          <FormItem label="预警风险:" prop="earlyWarning">
-            <Select v-model="model1" style="width: 200px">
-              <Option
-                v-for="item in cityList"
-                :value="item.value"
-                :key="item.value"
-                >{{ item.label }}</Option
-              >
-            </Select>
-          </FormItem>
-          <FormItem label="审核日期:" prop="checkBeginDate">
-            <DatePicker
-              type="daterange"
-              placement="bottom-end"
-              placeholder="请选择"
-            />
-          </FormItem>
-          <FormItem label="审核时间:" prop="contactPhone">
-            <TimePicker
-              type="timerange"
-              placement="bottom-end"
-              placeholder="请选择"
-            />
-          </FormItem>
-          <FormItem label="金额区间:" prop="cname">
-            <div class="numCount">
-              <Input v-model="formInline.cname" placeholder="请输入" />
-              <span style="margin: 0 15px">——</span>
-              <Input v-model="formInline.cname" placeholder="请输入" />
-            </div>
-          </FormItem>
-          <FormItem>
-            <Button type="primary" @click="searchData()">查询</Button>
+          <div class="searchItem">
+            <FormItem label="单据编号:" prop="code">
+              <Input v-model="formInline.code" placeholder="请输入单据编号" />
+            </FormItem>
+            <FormItem label="业务名称:" prop="type">
+              <Input v-model="formInline.type" placeholder="请输入业务名称" />
+            </FormItem>
+            <FormItem label="审核日期:" prop="checkBeginDate">
+              <DatePicker
+                style="width: 100%"
+                type="daterange"
+                placement="bottom-end"
+                placeholder="请选择"
+              />
+            </FormItem>
+          </div>
+          <div class="searchItem">
+            <FormItem label="审核结果:" prop="checkResult">
+              <Input placeholder="审核结果" />
+              <!-- <Select v-model="model1">
+                <Option
+                  v-for="item in cityList"
+                  :value="item.value"
+                  :key="item.value"
+                  >{{ item.label }}</Option
+                >
+              </Select> -->
+            </FormItem>
+            <FormItem label="预警风险:" prop="earlyWarning">
+              <Input placeholder="预警风险" />
+              <!-- <Select v-model="model1">
+                <Option
+                  v-for="item in cityList"
+                  :value="item.value"
+                  :key="item.value"
+                  >{{ item.label }}</Option
+                >
+              </Select> -->
+            </FormItem>
+
+            <FormItem label="金额区间:" prop="cname">
+              <div class="numCount">
+                <Input v-model="formInline.cname" placeholder="请输入" />
+                <span style="margin: 0 15px">——</span>
+                <Input v-model="formInline.cname" placeholder="请输入" />
+              </div>
+            </FormItem>
+          </div>
+
+          <FormItem style="display: flex; justify-content: flex-end">
+            <Button @click="searchData()">查询</Button>
             <Button @click="handleReset('formInline')" style="margin-left: 15px"
               >重置</Button
             >
+            <Button
+              style="margin-left: 15px"
+              @click="exported"
+              :disabled="idArr.length == 0"
+              >导出</Button
+            >
+            <Button @click="setting" style="margin-left: 15px">设置</Button>
           </FormItem>
         </Form>
       </Card>
 
       <Card style="width: 100%; margin-top: 20px">
-        <div class="addBox">
-          <Button type="primary" icon="ios-folder-open" @click="exported"
-            >导出</Button
-          >
-          <Button
-            type="primary"
-            icon="md-settings"
-            @click="setting"
-            style="margin-left: 15px"
-            >设置</Button
-          >
-        </div>
         <div class="tableList">
-          <el-table
+          <el-table ref='table'
             :data="tableData"
             border
             style="width: 100%"
             empty-text="暂无数据"
-            :header-cell-style="{ background: '#eef1f6', color: '#606266' }"
+            :header-cell-style="{
+              background: '#eef1f6',
+              color: '#606266',
+              fontWeight: 'normal',
+              fontSize: '12px',
+            }"
             @selection-change="handleSelectionChange"
+            show-summary
+            sum-text=" "
+            :summary-method="calculateSummary"
           >
-            <el-table-column type="selection" width="55" />
-            <el-table-column type="index" label="序号" align="center" />
+            <el-table-column type="selection" width="45" />
+            <el-table-column
+              type="index"
+              label="序号"
+              align="center"
+              width="45"
+            />
             <el-table-column label="操作" align="center">
               <template slot-scope="scope">
                 <el-button
@@ -141,7 +151,7 @@
             >
               <template slot-scope="scope">
                 <Progress
-                  :percent="Number(scope.row.ocrSchedule)"
+                  :percent="parseFloat(scope.row.ocrSchedule)"
                   :stroke-width="5"
                 />
               </template>
@@ -213,6 +223,48 @@
                 }}</span>
               </template>
             </el-table-column>
+            <div slot="append">
+              <div class="sum_footer xiaoji" ref="sum_xiaoji">
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit center">数据获取平均时长</div>
+                <div class="sum_footer_unit center">OCR识别平均时长</div>
+                <div class="sum_footer_unit center">规则审核平均时长</div>
+                <div class="sum_footer_unit center">审单平均时长</div>
+              </div>
+              <div class="sum_footer" ref="sum_heji">
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit"></div>
+                <div class="sum_footer_unit center">{{ tableSum.rpaDateAvg }}s</div>
+                <div class="sum_footer_unit center">{{ tableSum.ocrDateAvg }}min</div>
+                <div class="sum_footer_unit center">
+                  {{ tableSum.rulesDateAvg }}min
+                </div>
+                <div class="sum_footer_unit center">
+                  {{ tableSum.totalDateAvg }}min
+                </div>
+              </div>
+            </div>
           </el-table>
         </div>
         <el-row class="paginationStyle">
@@ -221,9 +273,11 @@
             @on-page-size-change="sizeChange"
             :current="page.currentPage"
             :total="page.totalElement"
+            prev-text="上一页"
+            next-text="下一页"
             show-elevator
             show-sizer
-            show-total
+            class-name="page-box"
           />
         </el-row>
       </Card>
@@ -232,7 +286,11 @@
     <el-dialog title="设置" :visible.sync="dialogFormVisible">
       <el-form :model="form" label-width="160px" center>
         <el-form-item label="选择列表展示字段：">
-          <el-checkbox-group v-model="form.type" @change="handleChange">
+          <el-checkbox-group
+            class="grid_3"
+            v-model="form.type"
+            @change="handleChange"
+          >
             <el-checkbox label="单据编号" name="type"></el-checkbox>
             <el-checkbox label="业务名称" name="type"></el-checkbox>
             <el-checkbox label="金额" name="type"></el-checkbox>
@@ -254,7 +312,7 @@
 </template>
 <script>
 import logJson from "@/dataJson/log.json";
-import store from "@/store";
+import axios from "axios";
 export default {
   data() {
     return {
@@ -275,7 +333,17 @@ export default {
         endMoney: "",
       },
       form: {
-        type: [],
+        type: [
+          "单据编号",
+          "业务名称",
+          "金额",
+          "审核日期",
+          "审核开始时间",
+          "审核结束时间",
+          "审核结果",
+          "预警风险",
+          "影像张数",
+        ],
       },
       tableData: [],
       cityList: [
@@ -293,6 +361,13 @@ export default {
         },
       ],
       model1: "",
+      idArr: [],
+      tableSum: {
+        rpaDateAvg: 0,
+        ocrDateAvg: 0,
+        rulesDateAvg: 0,
+        totalDateAvg: 0,
+      },
     };
   },
   created() {
@@ -300,55 +375,66 @@ export default {
       let { data } = logJson.data;
       this.tableData = data;
     }
-    this.query();
+  },
+  mounted() {
+    this.adjustWidth();
+    window.addEventListener("resize", this.adjustWidth.bind(this));
+    // this.query();
   },
   methods: {
     handleChange(value) {
       this.form.type = value;
     },
-    handleSelectionChange(val){
-      console.log(val,'454587542');
+    handleSelectionChange(val) {
+      let idArr = [];
+      val.map((item, index) => {
+        return idArr.push(item.id);
+      });
+      this.idArr = idArr;
     },
     query() {
-      let params = {
-        // id: store.state.user.id,
-        code: this.formInline.code.replace(/\s*/g, "") || "",
-        type: this.formInline.type.replace(/\s*/g, "") || "",
-        checkResult: this.formInline.checkResult.replace(/\s*/g, "") || "",
-        earlyWarning: this.formInline.earlyWarning.replace(/\s*/g, "") || "",
-        pageNum: this.page.currentPage,
-        pageSize: this.page.size,
-      };
-      getTableData(params).then((res) => {
-        let data = res.data;
-        if (data.code === 200) {
-          let showData = data.data;
-          this.tableData = showData.list;
-          this.page.totalElement = showData.total;
-          this.page.size = showData.pageSize;
-        }
+      const _this = this;
+      axios({
+        url: `http://10.15.196.127:7070/bill/page/${this.page.currentPage}/${this.page.size}`,
+        method: "POST",
+        data: {
+          code: this.formInline.code,
+          type: this.formInline.type,
+          checkResult: this.formInline.checkResult,
+          earlyWarning: this.formInline.earlyWarning,
+          beginMoney: this.formInline.beginMoney,
+          endMoney: this.formInline.endMoney,
+        },
+        success: (res) => {
+          if (res.code == 20000) {
+            _this.tableData = res.data.data;
+          }
+        },
       });
     },
     // 导出
-    exported() {},
+    exported() {
+      axios({
+        url: `http://10.15.196.127:7070/bill/export`,
+        method: "POST",
+        data: this.idArr,
+        success: (res) => {
+          console.log(res, "导出返回的");
+          // if (res.code == 20000) {
+          //   this.tableData = res.data.data;
+          // }
+        },
+      });
+    },
     // 设置
     setting() {
       this.dialogFormVisible = true;
     },
     submit() {},
     handleClick(row) {
-      this.$router.push({ path: "/logResult/logResult" });
-      // let path =
-      //   type == "view"
-      //     ? "/companyManage/viewCompany"
-      //     : "/companyManage/editCompany";
-      // this.$router.push({
-      //   path: path,
-      //   query: {
-      //     id: row.id,
-      //     type,
-      //   },
-      // });
+      this.$router.push({
+        path: `/logResult/logResult?billNumber=${row.code}`,
+      });
     },
     searchData() {
       this.page.currentPage = 1;
@@ -366,6 +452,89 @@ export default {
       this.page.size = size;
       this.query();
     },
+    calculateAvg(sum, length, key) {
+      const _this = this;
+      _this.tableSum[key] = parseFloat(sum) / parseFloat(length).toFixed(2);
+    },
+    calculateSummary(param) {
+      const _this = this;
+      const { columns, data } = param;
+      const sums = [];
+      columns.forEach((column, index) => {
+        const allRpaDateValues = data.map((item) =>
+          Number(item[column.property])
+        );
+        let sumColumn = 0;
+        if (!allRpaDateValues.every((value) => isNaN(value))) {
+          sumColumn = allRpaDateValues.reduce((prev, curr) => {
+            const value = Number(curr);
+            if (!isNaN(value)) {
+              return prev + curr;
+            } else {
+              return prev;
+            }
+          }, 0);
+        }
+        if (index === 13) {
+          sums[index] = "数据获取平均时长";
+          _this.calculateAvg(sumColumn, allRpaDateValues.length, "rpaDateAvg");
+          return;
+        }
+        if (index === 14) {
+          sums[index] = "OCR识别平均时长";
+          _this.calculateAvg(sumColumn, allRpaDateValues.length, "ocrDateAvg");
+          return;
+        }
+        if (index === 15) {
+          sums[index] = "规则审核平均时长\nww" + '<br/>' +'ww';
+          _this.calculateAvg(
+            sumColumn,
+            allRpaDateValues.length,
+            "rulesDateAvg"
+          );
+          return;
+        }
+        if (index === 16) {
+          sums[index] = `审单平均时长\nww`;
+          _this.calculateAvg(
+            sumColumn,
+            allRpaDateValues.length,
+            "totalDateAvg"
+          );
+          return;
+        }
+        sums[index] = "";
+        return;
+      });
+
+      return sums;
+    },
+
+    adjustWidth() {
+      this.$nextTick(() => {
+        if (
+          this.$refs &&
+          this.$refs.sum_xiaoji &&
+          this.$refs.sum_heji &&
+          this.$refs.table &&
+          this.$refs.table.$refs.headerWrapper
+        ) {
+          var width = getComputedStyle(
+            this.$refs.table.$refs.headerWrapper.querySelector("table")
+          ).width;
+          this.$refs.sum_xiaoji.style = "width:" + (width - 16) + 'px';
+          this.$refs.sum_heji.style = "width:" + (width - 16) + 'px';
+          Array.from(
+            this.$refs.table.$refs.headerWrapper.querySelectorAll("col")
+          ).forEach((n, i) => {
+            this.$refs.sum_xiaoji.children[i].style =
+              "width:" + (n.getAttribute("width")) + "px";
+            this.$refs.sum_heji.children[i].style =
+              "width:" + n.getAttribute("width") + "px";
+          });
+        }
+      });
+    },
   },
 };
 </script>
@@ -375,5 +544,49 @@ export default {
 }
 /deep/.el-dialog {
   width: 30%;
+}
+.searchItem {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  .ivu-form-item {
+    width: 28%;
+  }
+}
+/deep/.ivu-date-picker,
+.ivu-select {
+  flex: 1;
+}
+.grid_3 {
+  display: grid;
+  grid-template-columns: 30% 30% 30%;
+}
+/deep/.el-table__footer {
+  font-size: 12px !important;
+  display: none;
+}
+.page-box {
+}
+
+.sum_footer {
+  display: flex;
+  display: -webkit-flex;
+  line-height: 50px;
+  color: #606266;
+}
+.sum_footer_unit {
+  flex-grow: 1;
+  -webkit-flex-grow: 1;
+  // text-indent: 10px;
+  font-size: 12px !important;
+  // font-size: 14px;
+  // border: 1px solid #ebeef5;
+}
+.sum_footer_unit.center {
+  text-align: center;
+  // border-left: 1px solid #ebeef5;
+}
+.sum_footer.xiaoji {
+  border-bottom: 1px solid #ebeef5;
 }
 </style>
