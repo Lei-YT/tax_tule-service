5147651eac39af7ce21961a7e9899f57881f96ad
lyt <ldj_yt@outlook.com>
2021-05-05 19:45:28 +0800

    test: login

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -1,6 +1,6 @@
 import {
   login,
-  logout,
+  logout
 } from '@/api/user'
 import { setToken, getToken } from '@/libs/util'
 import { Notification } from 'element-ui'
@@ -21,44 +21,44 @@ export default {
     messageContentStore: {}
   },
   mutations: {
-    setAvatar(state, avatarPath) {
+    setAvatar (state, avatarPath) {
       state.avatarImgPath = avatarPath
     },
-    setId(state, id) {
+    setId (state, id) {
       state.id = id
     },
-    setUserName(state, name) {
+    setUserName (state, name) {
       state.userName = name
     },
-    setAdminNo(state, adminNo) {
+    setAdminNo (state, adminNo) {
       state.adminNo = adminNo
     },
-    setToken(state, token) {
+    setToken (state, token) {
       state.token = token
       setToken(token)
     },
-    setStationName(state, stationName) {
+    setStationName (state, stationName) {
       state.stationName = stationName
     },
-    setHasGetInfo(state, status) {
+    setHasGetInfo (state, status) {
       state.hasGetInfo = status
     },
-    setMessageCount(state, count) {
+    setMessageCount (state, count) {
       state.unreadCount = count
     },
-    setMessageUnreadList(state, list) {
+    setMessageUnreadList (state, list) {
       state.messageUnreadList = list
     },
-    setMessageReadedList(state, list) {
+    setMessageReadedList (state, list) {
       state.messageReadedList = list
     },
-    setMessageTrashList(state, list) {
+    setMessageTrashList (state, list) {
       state.messageTrashList = list
     },
-    updateMessageContentStore(state, { msg_id, content }) {
+    updateMessageContentStore (state, { msg_id, content }) {
       state.messageContentStore[msg_id] = content
     },
-    moveMsg(state, { from, to, msg_id }) {
+    moveMsg (state, { from, to, msg_id }) {
       const index = state[from].findIndex(_ => _.msg_id === msg_id)
       const msgItem = state[from].splice(index, 1)[0]
       msgItem.loading = false
@@ -72,37 +72,60 @@ export default {
   },
   actions: {
     // 登录
-    handleLogin({ commit }, { adminNo, password }) {
+    handleLogin ({ commit }, { adminNo, password }) {
       adminNo = adminNo.trim()
       return new Promise((resolve, reject) => {
-        login({
-          adminNo,
-          password
-        }).then(res => {
-          console.log(res,'res111111111111');
-          if (res.data.code != 0) {
-            Notification({
-              message: res.data.msg,
-              type: 'warning',
-              duration: 1300
-            })
-            return false
-          } else {
-            commit('setToken', res.data.data.token)
-            commit('setId', res.data.data.id)
-            commit('setUserName', res.data.data.name)
-            commit('setAvatar', res.data.data.imgUrl)
-            commit('setAdminNo', res.data.data.adminNo)
-            commit('setStationName', res.data.data.stationName)
-            resolve(res.data)
+        const tmpJson = {
+          'code': 0,
+          'msg': '操作成功.',
+          'data': {
+            'id': 1,
+            'stationName': 'admin',
+            'name': '超级管理员',
+            'imgUrl': 'http://pic.58pic.com/58pic/14/86/98/88X58PICz9T_1024.jpg',
+            'adminNo': 'admin',
+            'isEnable': 0,
+            'created_at': '2020-12-18 15:56:39',
+            'updated_at': '2020-08-22 14:21:11',
+            'deleted_at': null,
+            'token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHA6Ly8xMC43LjEuNjgvYXBpL2xvZ2luL2xvZ2luIiwiaWF0IjoxNjE5Njk4ODAzLCJleHAiOjE2MjIyOTA4MDMsIm5iZiI6MTYxOTY5ODgwMywianRpIjoiU1BrRGk4dVpLWm1yd1hGcSJ9.9T46DIJ2x7FNhofr78LFtQGbS8hUif55btM3UV_fNZQ'
           }
-        }).catch(err => {
-            reject(err)
-        })
+        }
+        commit('setToken', tmpJson.data.token)
+        commit('setId', tmpJson.data.id)
+        commit('setUserName', tmpJson.data.name)
+        commit('setAvatar', tmpJson.data.imgUrl)
+        commit('setAdminNo', tmpJson.data.adminNo)
+        commit('setStationName', tmpJson.data.stationName)
+        resolve(tmpJson)
+        // login({
+        //   adminNo,
+        //   password
+        // }).then(res => {
+        //   console.log(res,'res111111111111');
+        //   if (res.data.code != 0) {
+        //     Notification({
+        //       message: res.data.msg,
+        //       type: 'warning',
+        //       duration: 1300
+        //     })
+        //     return false
+        //   } else {
+        //     commit('setToken', res.data.data.token)
+        //     commit('setId', res.data.data.id)
+        //     commit('setUserName', res.data.data.name)
+        //     commit('setAvatar', res.data.data.imgUrl)
+        //     commit('setAdminNo', res.data.data.adminNo)
+        //     commit('setStationName', res.data.data.stationName)
+        //     resolve(res.data)
+        //   }
+        // }).catch(err => {
+        //     reject(err)
+        // })
       })
     },
     // 退出登录
-    handleLogOut({ state, commit }) {
+    handleLogOut ({ state, commit }) {
       return new Promise((resolve, reject) => {
         logout(state.token).then(() => {
           commit('setToken', '')
@@ -115,14 +138,14 @@ export default {
       })
     },
     // 此方法用来获取未读消息条数，接口只返回数值，不返回消息列表
-    getUnreadMessageCount({ state, commit }) {
+    getUnreadMessageCount ({ state, commit }) {
       getUnreadCount().then(res => {
         const { data } = res
         commit('setMessageCount', data)
       })
     },
     // 获取消息列表，其中包含未读、已读、回收站三个列表
-    getMessageList({ state, commit }) {
+    getMessageList ({ state, commit }) {
       return new Promise((resolve, reject) => {
         getMessage().then(res => {
           const { unread, readed, trash } = res.data
@@ -142,7 +165,7 @@ export default {
       })
     },
     // 根据当前点击的消息的id获取内容
-    getContentByMsgId({ state, commit }, { msg_id }) {
+    getContentByMsgId ({ state, commit }, { msg_id }) {
       return new Promise((resolve, reject) => {
         let contentItem = state.messageContentStore[msg_id]
         if (contentItem) {
@@ -157,7 +180,7 @@ export default {
       })
     },
     // 把一个未读消息标记为已读
-    hasRead({ state, commit }, { msg_id }) {
+    hasRead ({ state, commit }, { msg_id }) {
       return new Promise((resolve, reject) => {
         hasRead(msg_id).then(() => {
           commit('moveMsg', {
@@ -173,7 +196,7 @@ export default {
       })
     },
     // 删除一个已读消息到回收站
-    removeReaded({ commit }, { msg_id }) {
+    removeReaded ({ commit }, { msg_id }) {
       return new Promise((resolve, reject) => {
         removeReaded(msg_id).then(() => {
           commit('moveMsg', {
@@ -188,7 +211,7 @@ export default {
       })
     },
     // 还原一个已删除消息到已读消息
-    restoreTrash({ commit }, { msg_id }) {
+    restoreTrash ({ commit }, { msg_id }) {
       return new Promise((resolve, reject) => {
         restoreTrash(msg_id).then(() => {
           commit('moveMsg', {
