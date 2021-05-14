49e7013924823c18d815ad637e8813f9552a421d
lyt <ldj_yt@outlook.com>
2021-05-14 20:56:03 +0800

    error

--- src/plugin/error-store/index.js
+++ src/plugin/error-store/index.js
@@ -3,15 +3,17 @@ export default {
   install (Vue, options) {
     if (options.developmentOff && process.env.NODE_ENV === 'development') return
     Vue.config.errorHandler = (error, vm, mes) => {
-      let info = {
-        type: 'script',
-        code: 0,
-        mes: error.message,
-        url: window.location.href
-      }
-      Vue.nextTick(() => {
-        store.dispatch('addErrorLog', info)
-      })
+      // console.log('error catched',error)
+      vm.message = error
+      // let info = {
+      //   type: 'script',
+      //   code: 0,
+      //   mes: error.message,
+      //   url: window.location.href
+      // }
+      // Vue.nextTick(() => {
+      //   store.dispatch('addErrorLog', info)
+      // })
     }
   }
 }
