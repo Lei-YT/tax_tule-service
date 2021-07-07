9283d07622a3a3bac16d0b31475fd4dbecf5591a
lyt <ldj_yt@outlook.com>
2021-07-08 00:56:17 +0800

    修改密码

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -39,6 +39,22 @@
         </el-form-item>
         <el-form-item label="新密码：" prop="password">
           <el-input v-model="pwModify.password" show-password />
+          <Row
+            v-if="pwModify.password !== ''"
+            type="flex"
+            :gutter="16"
+            justify="start"
+          >
+            <Col>强度</Col>
+            <Col style="flex: 0 0 200px"
+              ><Progress
+                :percent="pwCheckPercent"
+                :stroke-color="pwCheckColor"
+                hide-info
+            /></Col>
+            <Col>{{ pwCheckText }}</Col>
+          </Row>
+          <!-- stroke-color -->
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -54,6 +70,35 @@
 import LoginForm from "_c/login-form";
 import { mapActions, mapMutations } from "vuex";
 import { passwordchange } from "@/api/user";
+function checkStrong(sValue) {
+  var modes = 0;
+  var txt = '低';
+  var color = "#ed4014"; // #ff9900 #19be6b
+  var percent = 33;
+  //正则表达式验证符合要求的
+  if (sValue.length < 1) return txt;
+  if (/\d/.test(sValue)) modes++; //数字
+  if (/[a-z]/.test(sValue)) modes++; //小写
+  if (/[A-Z]/.test(sValue)) modes++; //大写
+  if (/\W/.test(sValue)) modes++; //特殊字符
+  switch (modes) {
+    case 1:
+    case 2:
+      break;
+    case 3:
+      txt = '中';
+      color = '#ff9900';
+      percent = 66;
+      break;
+    case 4:
+      txt = '高';
+      txt = sValue.length < 8 ? '中' : '高';
+      color = txt === '高' ? '#19be6b' : '#ff9900';
+      percent = txt === '高' ? 100 : 66;
+      break;
+  }
+  return {txt, color, percent};
+}
 export default {
   components: {
     LoginForm,
@@ -65,8 +110,21 @@ export default {
       },
       set() {},
     },
+    newPw() {
+      return this.pwModify.password;
+    },
   },
   data() {
+    const validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请填写新密码"));
+      } else {
+        if (this.pwCheckText !== '高') {
+            callback(new Error("密码请设置8位以上, 需包含大写字母, 小写字母, 数字, 特殊字符"));
+        }
+        callback();
+      }
+    };
     return {
       pwModify: {
         // adminNo: "",
@@ -75,10 +133,23 @@ export default {
       },
       pwRules: {
         // adminNo: [{ required: true, message: "请填写账号" }],
-        password: [{ required: true, message: "请填写新密码" }],
         oldpassword: [{ required: true, message: "请填写旧密码" }],
+        password: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
       },
+      pwCheckColor: "#ed4014", // #ff9900 #19be6b
+      pwCheckText: "低",
+      pwCheckPercent: 33,
     };
+  },
+  watch: {
+    newPw(newValue, oldValue) {
+      const pwCheck = checkStrong(newValue);
+      this.pwCheckColor = pwCheck.color;
+      this.pwCheckText = pwCheck.txt;
+      this.pwCheckPercent = pwCheck.percent;
+    },
   },
   methods: {
     ...mapActions(["handleLogin"]),
@@ -110,7 +181,7 @@ export default {
               _this.$notify({
                 title: res.data.msg,
                 type: "success",
-                message: '请重新登录.',
+                message: "请重新登录.",
               });
               _this.setShowPWModify(false);
             } else {
