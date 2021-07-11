a8c13cbd2d606598a68202722a3150c5e4c9b769
lyt <ldj_yt@outlook.com>
2021-07-12 05:33:12 +0800

    不保留登录

--- src/router/index.js
+++ src/router/index.js
@@ -36,14 +36,14 @@ router.beforeEach((to, from, next) => {
         next({
             name: 'login' // 跳转到登录页
         })
-    } else if (!token && to.name == LOGIN_PAGE_NAME) {
-        // 未登陆且要跳转的页面是登录页
-        next() // 跳转
-    } else if (token && to.name == LOGIN_PAGE_NAME) {
-        // 已登录且要跳转的页面是登录页
-        next({
-            name: homeName // 跳转到homeName页
-        })
+    // } else if (!token && to.name == LOGIN_PAGE_NAME) {
+    //     // 未登陆且要跳转的页面是登录页
+    //     next() // 跳转
+    // } else if (token && to.name == LOGIN_PAGE_NAME) {
+    //     // 已登录且要跳转的页面是登录页
+    //     next({
+    //         name: homeName // 跳转到homeName页
+    //     })
     } else {
         // if (from.name == 'logResultIndex') {
         //   to.meta.isBack = true;
